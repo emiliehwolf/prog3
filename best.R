44d5ed22e67e65fe3cafc258306f6ba1a6578e52
Emilie H Wolf <emilie.h.wolf@gmail.com>
2015-03-29 12:50:23 -0500

    Update best.R

--- best.R
+++ best.R
@@ -1,7 +1,7 @@
 best <- function(state, outcome) {
     ## Read outcome data
     df <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
-    df <- data.frame(df[,2],df[,7],df[,11],df[,17],df[,23])
+    dfslim <- data.frame(df[,2],df[,7],df[,11],df[,17],df[,23])
 
     ##colnames(df) <- c("hosp","state","ha","hf","pn") ##unneccessary
 
@@ -16,7 +16,19 @@ best <- function(state, outcome) {
     ##    stop() ###
     ##}
 
-    lowest <- min(as.numeric(df[,x]), na.rm=TRUE)
-    lowest
+    dfstate <- dfslim[dfslim[,2]==state,]  ##subsetting just the states in question
+    minamount <- min(as.numeric(dfstate[,x]), na.rm=TRUE)
+
+    ##    lowest <- df[which.min(dfstate[,x]),2]
     
+    besthospitals <- vector(mode="character", length=0)
+    
+    for (i in nrow(dfstate)) {
+        if(dfstate[i,x]==minamount) {
+            besthospitals <- c(besthospitals,dfstate[i,1])
+        }
+    }
+    
+    lowest <- min(besthospitals)
+    lowest
 }
